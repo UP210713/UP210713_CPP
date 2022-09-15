8ad1f8ff8a66fa08bfe9af333e59b276b5406f2d
Samuel Salvador Soto Torres <up210713@alumnos.upa.edu.mx>
2022-09-15 09:54:08 -0500

    Actualizacion

---
+++ U1/01-Hola.cpp
@@ -0,0 +1,21 @@
+/*
+Date: 12/09/2022
+Author: Samuel Salvador Soto Torres  
+Email: up210713@alumnos.upa.edu.mx
+Description: My first program in C++, enjoy this program
+Last modification: Mi primer programa en c++
+*/
+
+//----------------PREPOCESOR DIRECTIVES---------------//
+//Input/Output library between the screen and keyboard
+#include <iostream>
+
+//--------------- MAIN FUNCTION ----------------
+
+int main(){
+
+    std::cout<<"Hello world, welcome to the pollos hermanos rocha, my name is Samuel but you can call me Sam"<<std::endl;
+
+    //Return 0 means that the program ends succestully
+    return 0;    
+}
