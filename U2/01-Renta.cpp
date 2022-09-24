e741aa69e28609bd07dc3515020d7d98a3fea8ed
Samuel Salvador Soto Torres <up210713@alumnos.upa.edu.mx>
2022-09-23 22:22:35 -0500

    Corregido

--- U2/01-Renta.cpp
+++ U2/01-Renta.cpp
@@ -11,31 +11,32 @@ int main(){
     cout<<"Ingresa la renta que generaste este mes: "<<endl;
     cout<<"$ ";
     cin>> Renta;
-     if ( Renta<=9999)
-     {
-        totalAPagar=Renta*0.05;
-        cout<<"Tu total a pagar de impuesto es: $ "<<totalAPagar<<endl;
-     }
-        else if (Renta>= 10000 & Renta <=19999)
+         if ( Renta<=9999)
         {
-            totalAPagar=Renta*0.15;
-            cout<<"Tu total a pagar de impuesto es: $ "<<totalAPagar<<endl;
+        totalAPagar=Renta*0.05;
+  
         }
-            else if (Renta>=20000 & Renta <=34999)
+            else if (Renta>= 10000 & Renta <=19999)
             {
-                totalAPagar=Renta*0.20;
-                cout<<"Tu total a pagar de impuesto es: $ "<<totalAPagar<<endl;
+            totalAPagar=Renta*0.15;
+   
             }
-                else if (Renta>=35000 & Renta <= 59999)
+                else if (Renta>=20000 & Renta <=34999)
                 {
-                    totalAPagar=Renta*0.30;
-                     cout<<"Tu total a pagar de impuesto es: $ "<<totalAPagar<<endl;
+                totalAPagar=Renta*0.20;
+                   
                 }
-                    else if (Renta>=60000)
+                    else if (Renta>=35000 & Renta <= 59999)
                     {
-                         totalAPagar=Renta*0.45;
-                         cout<<"Tu total a pagar de impuesto es: $ "<<totalAPagar<<endl;
+                    totalAPagar=Renta*0.30;
+   
                     }
-    
+                        else if (Renta>=60000)
+                        {
+                         totalAPagar=Renta*0.45;
+                            
+                        }
+    cout<<"Tu total a pagar de impuesto es: "<<totalAPagar<<endl;
+
 return 0;                
 }
