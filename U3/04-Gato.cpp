64f148747e822c7734097872dbf4e7adafe6b781
Samuel <up210713@alumnos.upa.edu.mx>
2022-11-10 10:32:04 -0600

    Gato

--- U3/04-Gato.cpp
+++ U3/04-Gato.cpp
@@ -1,39 +1,36 @@
 #include <iostream>
 
-// Use of namespace to avoid std::
+//Use of the namespace to avoid the std::
 using namespace std;
-void hacertablero();
-int main()
-{
-    char estructuragato[6][11];
-    for (int row = 0; row < 6; row++)
-    {
-        for (int  col = 0; col < 11; col++)
-        {
 
-            if (col == 3 || col == 7){
-                estructuragato[row][col]='|';
-            }else if (row==1 || row == 3){
-                estructuragato[row][col]='_';
-            }
-            else {
-               estructuragato[row][col]=' '; 
-            }
-        }
-    }
-    for (int row = 0; row < 6; row++)
+int row=0, col=0;
+char estructuraGato[6][11];
+
+int main(){
+    bool GameOver = false;
+    int jugada;
+    bool CasillaOcupada;
+    int mode;
+    cout << "Welcome to the game of toc tac toe"<<endl;
+    cout << "Please chose your mode to play"<<endl;
+    cout << "1) Player 1 vs CPU"<<endl;
+    cout << "2) Player 1 vs Player 2"<<endl;
+    cin>>mode;
+    if (mode==1)
     {
-        for (int  col = 0; col < 11; col++)
-        {
-            cout << estructuragato[row][col];
-        }
-        cout << endl;
+        
     }
-    return 0;
-    estructuragato[0][0]=1;
 }
-void hacertablero(){
-}
-void colocarJugada(int jugada){
-    char valorJugada;
+void hacerTablero(){
+   row=0;
+   col=0;
+   for (int Row = 0; Row < 6; Row++){
+        for (int Col = 0; Col < 11; Col++)
+        {
+        if (Col ==3 || Col ==7)
+        {
+            estructuraGato[Row][Col] = '|';
+        }
+        }
+   }
 }
