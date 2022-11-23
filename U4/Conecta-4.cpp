150b1aeedc6fe5f01b5f3d9bd1fd78f906c56bb2
Samuel <up210713@alumnos.upa.edu.mx>
2022-11-23 10:04:00 -0600

    Conect1

---
+++ U4/Conecta-4.cpp
@@ -0,0 +1,24 @@
+/*
+Date: 23/11/2022
+Author: Samuel Salvador Soto Torres
+        Jorge Emilio Santacruz Morales
+        Osiris Olimpia Martinez 
+
+Description: Conect 4
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int mainMenu();//Osiris
+void makeBoard();//Jorge
+int selectPlay();//Samuel
+bool checkPlay();//samuel
+
+
+// funcion principal 
+ int main(){
+
+ }
+
