caece461a028f645ea6e1ba9c93063ca2c4c5bfa
UP210263 <up210263@alumnos.upa.edu.mx>
2022-11-24 10:32:39 -0600

    MENU

--- U4/Conecta-4.cpp
+++ U4/Conecta-4.cpp
@@ -35,13 +35,13 @@ bool checkPlay();//samuel
 void gotoxy(int,int);
 
 char areaGame[7][7]={{'1','2','3','4','5','6','7'},{'8','9','10','11','12','13','14'},{'15','16','17','18','19','20','21'},
-                {'22','23','24','25','26','27','28'},{'29','30','31','32','33','34','35'}{'36','37','38','39','40','41','42'},{'43','44','45','46','47','48','49'}};
+                {'22','23','24','25','26','27','28'},{'29','30','31','32','33','34','35'},{'36','37','38','39','40','41','42'},{'43','44','45','46','47','48','49'}};
 
 
 // funcion principal 
  int main(){
-    menu();
-   // makeBoard();
+    mainMenu();
+    makeBoard();
 
  }
 
@@ -84,8 +84,9 @@ void gotoxy(int x,int y){
 int mainMenu(){
     gotoxy(15,3);
     int gameMode;
-    cout<<"Welcome to Connect 4"<<endl;
-    cout<<"Choose the game mode: 1.vs the PC or 2.vs another player  "
+    cout<<"************Welcome to Connect 4****************"<<endl;
+    cout<<"Choose the game mode: 1.vs the PC or 2.vs another player  ";
     cin>>gameMode;
     return gameMode;
 }
+
